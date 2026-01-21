a8bf0e3a49a590e514ff5d17185276e23957741d
Avinash Kumar <avinash.skumar@ascendion.com>
2026-01-21 21:55:20 +0530

    fixed code

--- frontend/src/lib/fcm.ts
+++ frontend/src/lib/fcm.ts
@@ -1,5 +1,5 @@
 import { getToken } from "firebase/messaging";
-import { messaging } from "./firebase";
+import { messagingPromise } from "./firebase";
 
 const VAPID_KEY =
   "BJm8t0K2LKC3k6QMQ-AQ8aZO6qcYGIb721l_ggbtW2KcdoxWSaneMrAmdsdvNOkfMCwQVhZ7Uro_qGjXmgJS5o8";
@@ -9,6 +9,12 @@ export async function getFcmToken(): Promise<string> {
 
   if (permission !== "granted") {
     throw new Error("Notification permission denied");
+  }
+
+  const messaging = await messagingPromise;
+
+  if (!messaging) {
+    throw new Error("FCM is not supported in this browser.");
   }
 
   const token = await getToken(messaging, {
